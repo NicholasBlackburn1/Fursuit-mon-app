107694a9a9d5331c5ceb9bc740940a449b9cb7d5
NickyBlackburn <nickblackburn02@gmail.com>
2023-03-03 19:26:31 -0500

    added the stylesheet

--- src/screens/SigninScreen/SigninScreen.tsx
+++ src/screens/SigninScreen/SigninScreen.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+/***
+ * 
+ * this is the sign in screen 
+ * it displays logo and login buttons  email and username
+ */
+
+import {View, Text, Image, StyleSheet} from 'react-native';
 import React from 'react'
+
+import Logo from '../../../assets/images/logo.png'
+
 
 const SigninScreen = () => {
   return (
     <View> 
-    
+    <Image source={Logo} style={styles.logo}></Image>
       <Text> help </Text>
-    
+
     </View>
   )
 }
