423775c169ad0a4b92c159eee6b7e9ef49be9106
NickyBlackburn <nickblackburn02@gmail.com>
2023-03-03 22:57:10 -0500

    working on adding social sign in buttons

---
+++ src/componets/SocialSignInButtons/SocialSignin.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SocialSignin = () => {
+  return (
+    <View>
+      <Text>SocialSignin</Text>
+    </View>
+  )
+}
+
+export default SocialSignin
