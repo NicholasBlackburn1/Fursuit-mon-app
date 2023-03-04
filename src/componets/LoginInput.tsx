7af1bbb121cf9b5494c167d580755fb91d62950d
NickyBlackburn <nickblackburn02@gmail.com>
2023-03-03 19:44:26 -0500

    got style bade setup in login input?

--- src/componets/LoginInput.tsx
+++ src/componets/LoginInput.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
 
 const LoginInput = () => {
   return (
     <View>
       <Text>LoginInput</Text>
+      <TextInput placeholder='placeholder'/>
     </View>
-  )
+  );
 }
+
+
+// allows me to set the style of the componet 
+const styles = StyleSheet.create({
+
+  container:{
+    
+  },
+
+  input:{
+
+  },
+});
+
 
 export default LoginInput
