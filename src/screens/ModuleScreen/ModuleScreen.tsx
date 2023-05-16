41ee3613695d6da9270f804bb3453526054ccf6a
NickyBlackburn <nickblackburn02@gmail.com>
2023-05-16 16:55:46 -0400

    got stuff working

--- src/screens/ModuleScreen/ModuleScreen.tsx
+++ src/screens/ModuleScreen/ModuleScreen.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import Cards from '../../componets/Cards';
 
 const ModuleScreen = ({ route }) => {
   const { module, sensors } = route.params;
   console.log("data "+module);
+    // Split the comma-separated string into an array
+    const sensorArray = sensors.split(',').map((sensor) => sensor.trim());
 
   return (
     <ScrollView>
       <View style={styles.root}>
         <Text style={styles.title}>{module} Data</Text>
+        {sensorArray.map((sensor, index) => (
+          <View key={index} style={styles.card}>
+           <Cards moduleName={sensor} sensorsName="" />
+          </View>
+        ))}
       </View>
     </ScrollView>
   );
