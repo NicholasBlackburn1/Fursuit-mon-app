11a0fbece043b5ecc284474acf76c7f5035006ef
NickyBlackburn <nickblackburn02@gmail.com>
2023-05-16 17:29:22 -0400

    got stuff working on module data page

--- src/screens/ModuleScreen/ModuleScreen.tsx
+++ src/screens/ModuleScreen/ModuleScreen.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Cards from '../../componets/Cards';
+import CardGuage from '../../componets/CardGuage/CardGuage';
 
 const ModuleScreen = ({ route }) => {
   const { module, sensors } = route.params;
   console.log("data "+module);
+
+  const [jsonData, setJsonData] = useState([]);
+
+
+  useEffect(() => {
+    const fetchvitals = async () => {
+      try {
+        const response = await fetch('http://192.168.4.1/fursuit/api/v1.0/app/getTempData');
+        const data = await response.json();
+        const parsedData = JSON.parse(data[0]);
+
+        // parses all the sensor daya
+        const temp = parsedData.temp;
+        const hum = parsedData.humitity;
+        const rate = parsedData.rate;
+
+        const extractedData = {temp, hum, rate};
+        console.log('Fetched JSON data:', extractedData);
+        setJsonData(extractedData);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    };
+
+    fetchvitals();
+  }, []);
+
+
     // Split the comma-separated string into an array
     const sensorArray = sensors.split(',').map((sensor) => sensor.trim());
+
 
   return (
     <ScrollView>
@@ -15,6 +45,7 @@ const ModuleScreen = ({ route }) => {
         {sensorArray.map((sensor, index) => (
           <View key={index} style={styles.card}>
            <Cards moduleName={sensor} sensorsName="" />
+
           </View>
         ))}
       </View>
